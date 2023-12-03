97b895dfeb5cc765d98e13e449dc86ad22f969db
Khush Agarwal <98137724+khush2003@users.noreply.github.com>
2023-12-03 13:09:18 +0700

    Testing webcam

--- helper.py
+++ helper.py
@@ -277,6 +277,7 @@ def play_webcam(conf, snakeModel, speciesModel):
     Raises:
         None
     """
+    st.camera_input("Take a picture")
     st.warning("#### ⚠️ Please read the wraning below before proceeding.")
     with st.expander("Note: Webcam will not work on deployed app. Please run the app locally to use the webcam. See instructions by exapnding this"):
         st.warning("If you are running this app locally, please ignore this message.")
