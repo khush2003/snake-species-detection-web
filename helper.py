499c863261a0464ccdc4fea0879badac107f6dd0
Khush Agarwal <98137724+khush2003@users.noreply.github.com>
2023-12-03 10:35:11 +0700

    Video

--- helper.py
+++ helper.py
@@ -206,8 +206,8 @@ def play_youtube_video(conf, speciesModel: YOLO, snakeModel:YOLO):
     if st.sidebar.button('Detect Objects'):
         try:
             yt = YouTube(source_youtube)
-            ext = ".mkv"
-            stream = yt.streams.filter(file_extension="mkv", res=720).first()
+            ext = ".avi"
+            stream = yt.streams.filter(file_extension="avi", res=720).first()
             vid_cap = cv2.VideoCapture(stream.url)
             results_display = st.expander("Detection Results")
 
@@ -215,7 +215,7 @@ def play_youtube_video(conf, speciesModel: YOLO, snakeModel:YOLO):
             fps = vid_cap.get(cv2.CAP_PROP_FPS) / counter_max
             frame_width = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
             frame_height = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            codec = int(vid_cap.get(cv2.CAP_PROP_FOURCC))
+            codec = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
             vid_writer = cv2.VideoWriter(f'{file_id}{ext}',  
                                 codec, 
                                 fps, (frame_width, frame_height)) 
@@ -257,7 +257,7 @@ def play_youtube_video(conf, speciesModel: YOLO, snakeModel:YOLO):
                 video_bytes = file.read()
             st.video(video_bytes)
             progress_bar.progress(100, "Done!")
-            # os.remove(saved_file_path)
+            os.remove(saved_file_path)
 
         except Exception as e:
             st.sidebar.error("Error loading video: " + str(e))
@@ -353,9 +353,8 @@ def play_stored_video(conf, model):
         with open(video_path, "rb") as file:
             video_bytes = file.read()
         st.video(video_bytes)
-
-    ext = '.mkv'
-
+    
+    ext = '.avi'
     
     if st.sidebar.button('Detect Video Objects'):
         progress_bar = st.progress(0, "Detecing Objects...")
@@ -370,7 +369,7 @@ def play_stored_video(conf, model):
         fps = vid_cap.get(cv2.CAP_PROP_FPS) / counter_max
         frame_width = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         frame_height = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        codec = int(vid_cap.get(cv2.CAP_PROP_FOURCC))
+        codec = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
         vid_writer = cv2.VideoWriter(f'{file_id}{ext}',  
                             codec, 
                             fps, (frame_width, frame_height)) 
@@ -407,13 +406,14 @@ def play_stored_video(conf, model):
                 vid_cap.release()
                 vid_writer.release()
                 break
+        print(ext)
         saved_file_path = Path(f'{file_id}{ext}')
         st.sidebar.success(f"Prediction Completed!")
         with open(saved_file_path, "rb") as file:
             video_bytes = file.read()
         st.video(video_bytes)
         progress_bar.progress(100, "Done!")
-        # os.remove(saved_file_path)
+        os.remove(saved_file_path)
     
         
         
